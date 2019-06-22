d5e0db34fd47b8b00eac7438bcef6f411adb638b
tom-hopley <tom.hopley1@gmail.com>
2019-06-22 02:49:56 +0100

    responsiveness

--- TaskList2/dom.js
+++ TaskList2/dom.js
@@ -16,26 +16,28 @@ class DOM {
     <article class="message ${color}" id="task-${id}">
     <div class="message-body level">
       <div class="level-left">
-        <div class="level-item has-text-centered">
+        <div id ="task-box"class="level-item has-text-centered">
           <span class="level-item is-size-4 ">${text}</span>
           </div>
       </div>
       <div class="level-right">
         <div class="level-item">
-          <p class="control"> <a data-id="${id}" class="edit-btn button is-inline">Edit</a></p>
+          <p class="control desktop-btn"> <a data-id="${id}" class="edit-btn button is-inline">Edit</a></p>
         </div>
 
 
     <div class="level-item">
       <div class="field has-addons">
+      <p class="control mobile-btn"> <a data-id="${id}" class="edit-btn button is-inline">Edit</a></p>
         <p class="control"> <a data-id="${id}" class="up-btn button is-inline"><i class="fas fa-chevron-up"></i>"</a></p>
         <p class="control"> <a data-id="${id}" class="down-btn button is-inline"><i class="fas fa-chevron-down"></i></a></p>
+        <p class="control mobile-btn"> <a data-id="${id}" class="delete-btn button is-inline is-danger"><i class="fas fa-times"></i></a></p>
       </div>
     </div>
 
   <div class="level-right">
     <div class="level-item">
-      <p class="control"> <a data-id="${id}" class="delete-btn button is-inline is-danger">Remove</a></p>
+      <p class="control desktop-btn"> <a data-id="${id}" class="delete-btn button is-inline is-danger">Remove</a></p>
     </div>
   </div>
 </div>
